be33522514b09975716791322128943ed8c4fdb0
amj52066 <judith.amrhein@web.de>
2015-08-11 13:40:05 +0200

    added StaticBlocks

---
+++ app/src/main/java/objects/StaticBlock.java
@@ -0,0 +1,11 @@
+package objects;
+
+/**
+ * Created by judith on 11.08.2015.
+ */
+public class StaticBlock extends Obj {
+
+    public StaticBlock(double x, double y, double w, double h){
+        super(x, y,w,h);
+    }
+}
